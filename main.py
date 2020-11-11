dd3e4be9909d63ffb437b99bb5287858f3525c91
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2020-11-11 07:30:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 1:Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
-
+#by rearrange the code
 # Can you help her?
 
 def greet(name):
